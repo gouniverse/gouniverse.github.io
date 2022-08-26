21fcf380581edb0bb8d50e1fdbf77d42d4093d11
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-26 07:20:59 +0100

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,246 +1,258 @@
-<html>
-  <body>
-    <h1>
-      GoUniverse
-    </h1>
-    
-    <hr />
-    
-    <p>
-      GoUniverse provides packages for GoLang
-    </p>
-    
-    
-    <!-- START: API -->
-    <h2>
-      API
-    </h2>
-    <p>
-      API responses to speed up API development
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/api" target="_blank">https://github.com/gouniverse/api</a>
-    </p>
-    <!-- END: API -->
-    
-    <!-- START: BCSV -->
-    <h2>
-      BCSV
-    </h2>
-    <p>
-      BCSV parser
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/bcsv" target="_blank">https://github.com/gouniverse/bcsv</a>
-    </p>
-    <!-- END: BCSV -->
-    
-    <!-- START: CacheStore -->
-    <h2>
-      Cache Store
-    </h2>
-    <p>
-      Stores the cache to a SQL database
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/cachestore" target="_blank">https://github.com/gouniverse/cachestore</a>
-    </p>
-    <!-- END: CacheStore -->
-    
-    <!-- START: Captcha -->
-    <h2>
-      Captcha
-    </h2>
-    <p>
-      Image challenge to tell bots from humans
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/captcha" target="_blank">https://github.com/gouniverse/captcha</a>
-    </p>
-    <!-- END: Captcha -->
-    
-    
-    <!-- START: CMS -->
-    <h2>
-      Cache Store
-    </h2>
-    <p>
-      CMS for GoLang
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/cms" target="_blank">https://github.com/gouniverse/cms</a>
-    </p>
-    <!-- END: CMS -->
-    
-    <!-- START: EntityStore -->
-    <h2>
-      Entity Store
-    </h2>
-    <p>
-      Store arbitrary data in this EAV implementation for a SQL database
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/entitystore" target="_blank">https://github.com/gouniverse/entitystore</a>
-    </p>
-    <!-- END: EntityStore -->
-    
-    
-    <!-- START: HB -->
-    <h2>
-      HB (HTML Builder)
-    </h2>
-    <p>
-      Build HTML programatically directly in Golang
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/hb" target="_blank">https://github.com/gouniverse/hb</a>
-    </p>
-    <!-- END: HB -->
-    
-    
-    <!-- START: icons -->
-    <h2>
-      Icons
-    </h2>
-    <p>
-      Build icons directly in Golang. Icon sets included: Bootstrap and Boxicons
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/icons" target="_blank">https://github.com/gouniverse/icons</a>
-    </p>
-    <!-- END: Icons -->
-    
-    
-    <!-- START: LogStore -->
-    <h2>
-      Log Store
-    </h2>
-    <p>
-      Stores the logs to a SQL database
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/logstore" target="_blank">https://github.com/gouniverse/logstore</a>
-    </p>
-    <!-- END: LogStore -->
-    
-    
-    <!-- START: MetaStore -->
-    <h2>
-      Meta Store
-    </h2>
-    <p>
-      Stores meta values to any object to a SQL database
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/metastore" target="_blank">https://github.com/gouniverse/metastore</a>
-    </p>
-    <!-- END: MetaStore -->
-    
-    
-    <!-- START: Password -->
-    <h2>
-      Password
-    </h2>
-    <p>
-      Convenient hashing and verification of passwords
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/password" target="_blank">https://github.com/gouniverse/password</a>
-    </p>
-    <!-- END: Password -->
-    
-    <!-- START: SessionStore -->
-    <h2>
-      Session Store
-    </h2>
-    <p>
-      Stores the session to a SQL database
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/sessionstore" target="_blank">https://github.com/gouniverse/sessionstore</a>
-    </p>
-    <!-- END: SessionStore -->
-    
-    
-    <!-- START: SettingStore -->
-    <h2>
-      Setting Store
-    </h2>
-    <p>
-      Stores the settings to a SQL database
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/sessionstore" target="_blank">https://github.com/gouniverse/sessionstore</a>
-    </p>
-    <!-- END: SettingStore -->
-    
-    <!-- START: TaskStore -->
-    <h2>
-      Task Store
-    </h2>
-    <p>
-      Stores tasks to a SQL database to be processed in the background
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/taskstore" target="_blank">https://github.com/gouniverse/taskstore</a>
-    </p>
-    <!-- END: TaskStore -->
-    
-    <!-- START: UID -->
-    <h2>
-      UID
-    </h2>
-    <p>
-      Unique human friendly identifiers.
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/uid" target="_blank">https://github.com/gouniverse/uid</a>
-    </p>
-    <!-- END: UID -->
-    
-    
-    <!-- START: Utils -->
-    <h2>
-      Utils
-    </h2>
-    <p>
-      Utility functions for Golang
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/utils" target="_blank">https://github.com/gouniverse/utils</a>
-    </p>
-    <!-- END: Utils -->
-    
-    
-    <!-- START: VaultStore -->
-    <h2>
-      Vault Store
-    </h2>
-    <p>
-      Stores the values encrypted with password to a SQL database aka "encryption-at-rest"
-    </p>
-    <p>
-      Website: 
-      <a href="https://github.com/gouniverse/vaultstore" target="_blank">https://github.com/gouniverse/vaultstore</a>
-    </p>
-    <!-- END: VaultStore -->
-    
-    
-  </body>
-</html>
+@extends('partials/layout', ['webpage_title'=>'Home'])
+
+@push('styles')
+<style>
+.SectionIntro {
+    background: #eee;
+    padding: 80px 20px;
+}
+</style>
+@endpush
+
+@section('content')
+<section class="SectionIntro">
+    <div class="container">
+      <h1>
+        GoUniverse
+      </h1>
+
+      <hr />
+
+      <p>
+        GoUniverse provides packages for GoLang
+      </p>
+
+
+      <!-- START: API -->
+      <h2>
+        API
+      </h2>
+      <p>
+        API responses to speed up API development
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/api" target="_blank">https://github.com/gouniverse/api</a>
+      </p>
+      <!-- END: API -->
+
+      <!-- START: BCSV -->
+      <h2>
+        BCSV
+      </h2>
+      <p>
+        BCSV parser
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/bcsv" target="_blank">https://github.com/gouniverse/bcsv</a>
+      </p>
+      <!-- END: BCSV -->
+
+      <!-- START: CacheStore -->
+      <h2>
+        Cache Store
+      </h2>
+      <p>
+        Stores the cache to a SQL database
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/cachestore" target="_blank">https://github.com/gouniverse/cachestore</a>
+      </p>
+      <!-- END: CacheStore -->
+
+      <!-- START: Captcha -->
+      <h2>
+        Captcha
+      </h2>
+      <p>
+        Image challenge to tell bots from humans
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/captcha" target="_blank">https://github.com/gouniverse/captcha</a>
+      </p>
+      <!-- END: Captcha -->
+
+
+      <!-- START: CMS -->
+      <h2>
+        Cache Store
+      </h2>
+      <p>
+        CMS for GoLang
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/cms" target="_blank">https://github.com/gouniverse/cms</a>
+      </p>
+      <!-- END: CMS -->
+
+      <!-- START: EntityStore -->
+      <h2>
+        Entity Store
+      </h2>
+      <p>
+        Store arbitrary data in this EAV implementation for a SQL database
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/entitystore" target="_blank">https://github.com/gouniverse/entitystore</a>
+      </p>
+      <!-- END: EntityStore -->
+
+
+      <!-- START: HB -->
+      <h2>
+        HB (HTML Builder)
+      </h2>
+      <p>
+        Build HTML programatically directly in Golang
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/hb" target="_blank">https://github.com/gouniverse/hb</a>
+      </p>
+      <!-- END: HB -->
+
+
+      <!-- START: icons -->
+      <h2>
+        Icons
+      </h2>
+      <p>
+        Build icons directly in Golang. Icon sets included: Bootstrap and Boxicons
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/icons" target="_blank">https://github.com/gouniverse/icons</a>
+      </p>
+      <!-- END: Icons -->
+
+
+      <!-- START: LogStore -->
+      <h2>
+        Log Store
+      </h2>
+      <p>
+        Stores the logs to a SQL database
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/logstore" target="_blank">https://github.com/gouniverse/logstore</a>
+      </p>
+      <!-- END: LogStore -->
+
+
+      <!-- START: MetaStore -->
+      <h2>
+        Meta Store
+      </h2>
+      <p>
+        Stores meta values to any object to a SQL database
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/metastore" target="_blank">https://github.com/gouniverse/metastore</a>
+      </p>
+      <!-- END: MetaStore -->
+
+
+      <!-- START: Password -->
+      <h2>
+        Password
+      </h2>
+      <p>
+        Convenient hashing and verification of passwords
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/password" target="_blank">https://github.com/gouniverse/password</a>
+      </p>
+      <!-- END: Password -->
+
+      <!-- START: SessionStore -->
+      <h2>
+        Session Store
+      </h2>
+      <p>
+        Stores the session to a SQL database
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/sessionstore" target="_blank">https://github.com/gouniverse/sessionstore</a>
+      </p>
+      <!-- END: SessionStore -->
+
+
+      <!-- START: SettingStore -->
+      <h2>
+        Setting Store
+      </h2>
+      <p>
+        Stores the settings to a SQL database
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/sessionstore" target="_blank">https://github.com/gouniverse/sessionstore</a>
+      </p>
+      <!-- END: SettingStore -->
+
+      <!-- START: TaskStore -->
+      <h2>
+        Task Store
+      </h2>
+      <p>
+        Stores tasks to a SQL database to be processed in the background
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/taskstore" target="_blank">https://github.com/gouniverse/taskstore</a>
+      </p>
+      <!-- END: TaskStore -->
+
+      <!-- START: UID -->
+      <h2>
+        UID
+      </h2>
+      <p>
+        Unique human friendly identifiers.
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/uid" target="_blank">https://github.com/gouniverse/uid</a>
+      </p>
+      <!-- END: UID -->
+
+
+      <!-- START: Utils -->
+      <h2>
+        Utils
+      </h2>
+      <p>
+        Utility functions for Golang
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/utils" target="_blank">https://github.com/gouniverse/utils</a>
+      </p>
+      <!-- END: Utils -->
+
+
+      <!-- START: VaultStore -->
+      <h2>
+        Vault Store
+      </h2>
+      <p>
+        Stores the values encrypted with password to a SQL database aka "encryption-at-rest"
+      </p>
+      <p>
+        Website: 
+        <a href="https://github.com/gouniverse/vaultstore" target="_blank">https://github.com/gouniverse/vaultstore</a>
+      </p>
+      <!-- END: VaultStore -->
+    </div>
+</section>
+
+@endsection
