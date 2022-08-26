13255061438ae8fd4c4b4711618f44995b14fe37
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-26 07:25:36 +0100

    Create header.blade.php

---
+++ source/partials/header.blade.php
@@ -0,0 +1,45 @@
+<style>
+    .navbar .navbar-brand {
+        font-size: 24px;
+        margin-right: 40px;
+    }
+
+    .navbar .nav-item {
+        font-size: 14px;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <b>GoUniverse</b>
+        </a>
+        
+        <button class="navbar-toggler" type="button" 
+            data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="/">
+                        Home
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <!-- END: Account -->
+                <!--
+                <li class="nav-item ">
+                    <a class="nav-link" href="/auth/login">
+                        Login
+                    </a>
+                </li>
+                -->
+            </ul>
+        </div>
+    </div>
+</nav>
